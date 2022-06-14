c7ae43c35d4b4263ddab7f7956483717676f7060
Alan Verissimo <alan_verissimo@hotmail.com>
2022-06-13 21:51:39 -0300

    Pagina de Perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import{ useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+
+import { Container } from './styles';
 
 export default function Profile() {
-    return <h1>Home</h1>;
+    const profile = useSelector(state => state.user.profile);
+
+    function handleSubmit(data) {}
+
+    return ( 
+    <Container>
+        <Form initialData={profile} onSubmit={handleSubmit}>
+            <Input name="name" placeholder="Nome Completo" />
+            <Input name="email" type="email" placeholder="Seu endereço de email" />
+            <hr />
+            
+            <Input type="password" name="oldPassword" placeholder="Sua senha atual" />
+            <Input type="password" name="password" placeholder="Nova Senha" />
+            <Input type="password" name="confirmPassword" placeholder="Confirmação de senha" />
+       
+        <button type ="submit">Atualizar perfil</button>
+         </Form> 
+         <button type="button">Sair do GoBarber</button>
+    </Container>
+    );
 }
